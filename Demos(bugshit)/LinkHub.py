e5bf97a302497e71c94b562aa633a5eba5f2fe19
Askir4 <lionsin43@gmail.com>
2025-01-27 11:09:56 +0100

    Update LinkHub.py

--- Demos(bugshit)/LinkHub.py
+++ Demos(bugshit)/LinkHub.py
@@ -32,6 +32,10 @@ def unifi_link():
   webbrowser.open("https://github.com/Askir4/Klassenshit")
 
 
+def youtube_link():
+    webbrowser.open("https://github.com/Askir4/Klassenshit")
+
+
   
   
 button_repo = tk.Button(root, text="Repo", font=font_cool, command=git_link, bg="grey")
@@ -39,14 +43,15 @@ button_Iutm = tk.Button(root, text="Iutm", font=font_cool, command=utm_link, bg=
 button_Processes = tk.Button(root, text="Tickets", font=font_cool, command=ticket_link, bg="grey")
 button_Hutm = tk.Button(root, text="Hutm", font=font_cool, command=utm_link, bg="grey")
 button_unifi = tk.Button(root, text="Unifi", font=font_cool, command=unifi_link, bg="grey")
-
+button_youtube = tk.Button(root, text="Youtube", font=font_cool, command=youtube_link, bg="grey")
 
 
 button_repo.grid(row=0, column=0, sticky="nswe")
 button_Iutm.grid(row=0, column=1, sticky="nswe")
 button_Processes.grid(row=0, column=2, sticky="nswe")
-button_unifi.grid(row=2, column=0, sticky="nswe")
-button_Hutm.grid(row=2, column=1, sticky="nswe")
+button_unifi.grid(row=1, column=0, sticky="nswe")
+button_Hutm.grid(row=1, column=1, sticky="nswe")
+button_youtube.grid(row=2, column=0, sticky="nswe")
 
 
 
