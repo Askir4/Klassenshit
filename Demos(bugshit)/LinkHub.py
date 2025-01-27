7b9b131faef5029f1e2ded93372fac131fbd3711
Askir4 <lionsin43@gmail.com>
2025-01-27 12:16:28 +0100

    Update LinkHub.py

--- Demos(bugshit)/LinkHub.py
+++ Demos(bugshit)/LinkHub.py
@@ -15,25 +15,33 @@ def git_link():
 
 
 def utm_link():
-  webbrowser.open("https://github.com/Askir4/Klassenshit")
+    webbrowser.open("https://github.com/Askir4/Klassenshit")
 
 
 def ticket_link():
-  webbrowser.open("https://github.com/Askir4/Klassenshit")
+    webbrowser.open("https://processes.com")
 
 
 
 def Hutm_link():
-  webbrowser.open("https://github.com/Askir4/Klassenshit")
+    webbrowser.open("https://github.com/Askir4/Klassenshit")
 
 
 
 def unifi_link():
-  webbrowser.open("https://github.com/Askir4/Klassenshit")
+    webbrowser.open("https://github.com/Askir4/Klassenshit")
 
 
 def youtube_link():
-    webbrowser.open("https://github.com/Askir4/Klassenshit")
+    webbrowser.open("https://youtube.com")
+
+
+def crwdstrike_link():
+    webbrowser.open("https://crowdstrike.com")
+
+
+def cve_link():
+    webbrowser.open("https://microsoft.com")
 
 
   
@@ -44,14 +52,17 @@ button_Processes = tk.Button(root, text="Tickets", font=font_cool, command=ticke
 button_Hutm = tk.Button(root, text="Hutm", font=font_cool, command=utm_link, bg="grey")
 button_unifi = tk.Button(root, text="Unifi", font=font_cool, command=unifi_link, bg="grey")
 button_youtube = tk.Button(root, text="Youtube", font=font_cool, command=youtube_link, bg="grey")
+button_crowdstrike = tk.Button(root, text="crowd", font=font_cool, command=crwdstrike_link, bg="grey")
+button_cve = tk.Button(root, text="cves",font=font_cool, command=cve_link, bg="grey" )
 
 button_Hutm.grid(row=0, column=0, sticky="nswe")
 button_Iutm.grid(row=0, column=1, sticky="nswe")
 button_Processes.grid(row=0, column=2, sticky="nswe")
-
 button_unifi.grid(row=1, column=0, sticky="nswe")
 button_youtube.grid(row=1, column=1, sticky="nswe")
 button_repo.grid(row=1, column=2, sticky="nswe")
+button_crowdstrike.grid(row=2, column=0, sticky="nswe")
+button_cve.grid(row=2, column=1, sticky="nswe")
 
 
 
@@ -60,7 +71,7 @@ for column in range (3):
   root.columnconfigure(column,weight=1)
 
 
-for row in range (2):
+for row in range (3):
   root.rowconfigure(row,weight=1 )
 
 
